58e6be02ae640ec3ae2c1e7a91dfa189bb51f7b9
Jaden Young <jaden.young44@gmail.com>
2018-04-25 02:59:36 -0500

    Added verison to cross-compile script

--- cross-compile.sh
+++ cross-compile.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker run --rm -v "$PWD":/go/src/app -w /go/src/app golang:1.10 \
+docker run --rm -v "$PWD":/go/src/app -w /go/src/app --env VERSION=$1 golang:1.10 \
   /bin/sh -c 'for GOOS in darwin linux windows; do \
                 for GOARCH in 386 amd64; do \
-                  go build -v -o publish-file-to-rabbit-$GOOS-$GOARCH 
+                  go build -v -o publish-file-to-rabbit-$VERSION-$GOOS-$GOARCH 
                 done 
               done'
